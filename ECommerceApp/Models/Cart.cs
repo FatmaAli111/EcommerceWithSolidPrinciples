5a2e92bcc66400e50d4381ede7684cc6e2f02c6a
rawan <rawan407504559@gmail.com>
2026-04-17 15:30:13 +0200

    Add core models: Product, CartItem, Cart, Customer

---
+++ ECommerceApp/Models/Cart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public List<CartItem> Items { get; private set; } = new List<CartItem>();
+        public Customer Customer { get; set; }
+
+        public Cart(int cartId, Customer customer)
+        {
+            CartId = cartId;
+            Customer = customer;
+        }
+
+        public void AddItem(Product product, int quantity)
+        {
+            var existing = Items.FirstOrDefault(i => i.Product.Id == product.Id);
+            if (existing != null)
+                existing.Quantity += quantity;
+            else
+                Items.Add(new CartItem(product, quantity));
+        }
+
+        public void RemoveItem(int productId)
+        {
+            Items.RemoveAll(i => i.Product.Id == productId);
+        }
+
+        public decimal GetTotal()
+        {
+            return Items.Sum(i => i.Subtotal);
+        }
+    }
+}
---
+++ ECommerceApp/Models/CartItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Models
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Subtotal => Product.Price * Quantity;
+
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+    }
+}
